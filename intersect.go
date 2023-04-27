a62599028329d2647bc3d0e313cc869cb403403e
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2023-04-27 21:06:28 +0300

    Update intersect.go

--- intersect.go
+++ intersect.go
@@ -1,5 +1,10 @@
 package dasugo
 
+// Intersect returns a new slice that contains only the elements that exist in both arr1 and arr2. The function takes two slices of type T and returns a new slice of type T.
+
+// The T type must be comparable, which means that it must be possible to determine equality between two values of the type using the == operator.
+
+// The function first creates a lookup slice (elementsLookUp) from arr1. Then, it iterates over arr2 and appends the items that exist in the lookup slice to the result slice.
 func Intersect[T comparable](arr1, arr2 []T) []T {
 	elementsLookUp := arr1
 	result := make([]T, 0)
